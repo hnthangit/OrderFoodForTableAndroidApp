dae77d137741f7c00bb9c46557c50dd737c38419
LL <mayukkr25@gmail.com>
2019-11-04 18:11:31 +0700

    feat: add drawer navigation for main screen

--- src/navigator/Navigator.js
+++ src/navigator/Navigator.js
@@ -1,15 +1,35 @@
 import BillHistoryScreen from '../screens/BillHistoryScreen';
 import IndexScreen from '../screens/IndexScreen';
 import TableScreen from '../screens/TableScreen';
-import {createStackNavigator} from 'react-navigation';
 
-const Navigator = createStackNavigator({
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+const DrawerNavigator = createDrawerNavigator({
+  //Can use name in here but i like label :)
   TableScreen: {
     screen: TableScreen,
+
+    //Name of item in drawer
+    navigationOptions: {
+      drawerLabel: 'Danh sách bàn',
+    },
   },
+
   BillHistoryScreen: {
     screen: BillHistoryScreen,
+    navigationOptions: {
+      drawerLabel: 'Lịch sử hóa đơn',
+    },
+  },
+  IndexScreen: {
+    screen: IndexScreen,
+    navigationOptions: {
+      drawerLabel: 'Màn hình chính',
+      headerStyle: {backgroundColor: '#4C3E54'},
+    },
   },
 });
+
+const Navigator = createAppContainer(DrawerNavigator);
 
 export default Navigator;
